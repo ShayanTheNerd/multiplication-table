a1cfce8ebbec40d15d006a3418dcaa82717f7456
Shayan Zamani <sh.zamani384@gmail.com>
2022-07-12 19:43:53 +0430

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,14 +3,16 @@ const domStrings = {
     headerNumbersRow: 'headerNumbers',
     selectedClass: 'selected',
     alertBox: 'smallScreenAlertBox',
+    lightBlueBg: 'bg-light-blue',
 }
 
 // Declare variables
 const tds = document.getElementsByTagName('td');
 const ths = document.getElementsByTagName('th');
+const trs = document.getElementsByTagName('tr');
 
 // Delete small-screen-alert-box on click
-document.getElementById(domStrings.alertBox).addEventListener('click', function () {
+document.getElementById(domStrings.alertBox).addEventListener('click', () => {
     this.style.display = 'none';
 });
 
@@ -23,7 +25,7 @@ function toggleSelectedOnTds(tableCell, toggleState) {
     }
 }
 
-// Add/remove 'selected' class for relevant 'th' elements
+// Add/remove 'selected' class for relevant 'th' elements and add/remove light background highlighter
 function toggleSelectedOnHeaderCells(tdCell, toggleState) {
     // An empty array for all sibling 'td' elements in a row
     const rowmates = [];
@@ -31,19 +33,39 @@ function toggleSelectedOnHeaderCells(tdCell, toggleState) {
     // All sibling 'td' elements in a row
     const siblingTds = tdCell.parentElement.getElementsByTagName('td');
 
-    // Populate 'rowmates' array
-    for (const tdCell of siblingTds) {
-        rowmates.push(tdCell.textContent);
+    for (const cellItem of siblingTds) {
+        // Populate 'rowmates' array
+        rowmates.push(cellItem.textContent);
+
+        // Add light-background highlighter to the relevant row
+        if (Number(cellItem.textContent) < Number(tdCell.textContent)) {
+            cellItem.classList.add(domStrings.lightBlueBg);
+        }
     }
 
     // Index of the current 'td' element
     const currentTdIndex = rowmates.indexOf(tdCell.textContent);
+
+    // Add light-background highlighter to the relevant cols
+    for (let i = 1; i < 9; i++) {
+        if (Number(trs[i].getElementsByTagName('td')[currentTdIndex].textContent) < Number(tdCell.textContent)) {
+            trs[i].getElementsByTagName('td')[currentTdIndex].classList.add(domStrings.lightBlueBg);
+        }
+    }
 
     // Select relevant 'th' element based on the index of the current 'td' element
     if (toggleState === 'add') {
         document.getElementById(domStrings.headerNumbersRow).getElementsByTagName('th')[(currentTdIndex + 1)].classList.add(domStrings.selectedClass);
     } else if (toggleState === 'remove') {
         document.getElementById(domStrings.headerNumbersRow).getElementsByTagName('th')[(currentTdIndex + 1)].classList.remove(domStrings.selectedClass);
+
+        // Remove light-background highlighter from all cols and rows
+        for (const cellItem of siblingTds) {
+            cellItem.classList.remove(domStrings.lightBlueBg);
+        }
+        for (let i = 1; i < 9; i++) {
+            trs[i].getElementsByTagName('td')[currentTdIndex].classList.remove(domStrings.lightBlueBg);
+        }
     }
 }
 
