161afa27e22891dbe38a623d7bd421be6aa61394
ShayanTheNerd <sh.zamani384@gmail.com>
2023-03-30 18:01:36 +0330

    Full code refactor & features for mobile support

--- scripts.js
+++ scripts.js
@@ -1,99 +1,117 @@
-// Get all id/classes from DOM
-const domStrings = {
-    headerNumbersRow: 'headerNumbers',
-    selectedClass: 'selected',
-    alertBox: 'smallScreenAlertBox',
-    lightBlueBg: 'bg-light-blue',
+'use strict';
+
+// global variable
+const tableTopHeaderRow = document.querySelector('#tableTopHeaderRow');
+
+// dynamically generate table cells
+{
+	const TABLE_COL_ROW_COUNT = 10;
+	const tableBody = document.querySelector('#tableBody');
+
+	// create and insert top heading cells
+	for (let i = 1; i <= TABLE_COL_ROW_COUNT; i++) tableTopHeaderRow.insertAdjacentHTML('beforeend', `<th>${i}</th>`);
+
+	// create and insert rows in the table body
+	for (let i = 1; i < TABLE_COL_ROW_COUNT; i++) tableBody.insertAdjacentHTML('beforeend', '<tr></tr>');
+
+	// create variables to keep track of row and column count
+	let colCounter = 0; // the first column
+	let rowCounter = 1; // the first body row (the table row is the header row)
+
+	// create variables to store header row cells and table body rows
+	const tableTopHeaderRowCells = [...tableTopHeaderRow.children];
+	const tableBodyRows = [...document.querySelectorAll('#tableBody tr')];
+
+	// generate table body cells
+	while (rowCounter < TABLE_COL_ROW_COUNT) {
+		const value = +tableTopHeaderRowCells[rowCounter].textContent * +tableTopHeaderRowCells[colCounter].textContent;
+		const maxRowValue = +tableTopHeaderRowCells.at(-1).textContent * +tableTopHeaderRowCells[rowCounter].textContent;
+
+		const insertNewCell = markup => tableBodyRows[rowCounter - 1].insertAdjacentHTML('beforeend', markup);
+
+		// insert 'th' instead of 'td' at the beginning of each row
+		const cellMarkup = colCounter === 0 ? `<th>${value}</th>` : `<td>${value}</td>`;
+		insertNewCell(cellMarkup);
+
+		if (value === maxRowValue) {
+			colCounter = 0; // reset the column count to 0 for the next row
+			rowCounter++; // move to the next row
+			continue;
+		}
+
+		colCounter++; // move to the next column
+	}
 }
 
-// Declare variables
-const tds = document.getElementsByTagName('td');
-const ths = document.getElementsByTagName('th');
-const trs = document.getElementsByTagName('tr');
+// init the app based on user's device
+{
+	// detect user's device
+	const { userAgent: deviceInfo } = navigator;
+	const deviceIsDesktop = !(/Mobi|Android/i.test(deviceInfo) || /iPhone|iPad|iPod/i.test(deviceInfo));
 
-// Delete small-screen-alert-box on click
-document.getElementById(domStrings.alertBox).addEventListener('click', () => {
-    this.style.display = 'none';
-});
+	if (!deviceIsDesktop) {
+		const warningModal = document.querySelector('#warningModal');
+		const closeModalBtn = document.querySelector('#closeModalBtn');
 
-// Add/remove 'selected' class for 'td' elements
-function toggleSelectedOnTds(tableCell, toggleState) {
-    if (toggleState === 'add') {
-        tableCell.parentElement.getElementsByTagName('th')[0].classList.add(domStrings.selectedClass);
-    } else if (toggleState === 'remove') {
-        tableCell.parentElement.getElementsByTagName('th')[0].classList.remove(domStrings.selectedClass);
-    }
+		warningModal.showModal();
+
+		closeModalBtn.addEventListener('click', () => warningModal.close());
+	}
+
+	const table = document.querySelector('#table');
+	const interaction = deviceIsDesktop ? 'mouseover' : 'click';
+
+	// mark corresponding table cells
+	table.addEventListener(interaction, event => {
+		const { target, currentTarget } = event;
+
+		if (target === currentTarget) return;
+
+		clearMarkedCells();
+		selectCell(target);
+		highlightRelatedCells(target);
+	});
+
+	// clear table when clicking or navigating mouse outside of it
+	['click', 'mouseout'].forEach(event => {
+		document.documentElement.addEventListener(event, e => !e.target.closest('#table') && clearMarkedCells());
+	});
 }
 
-// Add/remove 'selected' class for relevant 'th' elements and add/remove light background highlighter
-function toggleSelectedOnHeaderCells(tdCell, toggleState) {
-    // An empty array for all sibling 'td' elements in a row
-    const rowmates = [];
+// helper functions
+function highlightRelatedCells(cell) {
+	const tabletableTopHeaderRowCells = [...tableTopHeaderRow.children];
 
-    // All sibling 'td' elements in a row
-    const siblingTds = tdCell.parentElement.getElementsByTagName('td');
+	if (cell.tagName === 'TH') return selectCell(tabletableTopHeaderRowCells[0]);
 
-    for (const cellItem of siblingTds) {
-        // Populate 'rowmates' array
-        rowmates.push(cellItem.textContent);
+	// get coordinates of current cell and its neighbors
+	const cellValue = +cell.textContent;
+	const cellRowmates = [...cell.closest('tr').children].filter(item => item.tagName === 'TD'); // all 'td' elements in the same row
+	const cellIndexInRow = cellRowmates.map(item => +item.textContent).indexOf(cellValue); // index of 'cell' in the same row
+	const rowsInTableBody = [...cell.closest('#tableBody').querySelectorAll('tr')]; // all rows in the body of the table
+	const cellColmates = rowsInTableBody.map(row => row.querySelector(`td:nth-of-type(${cellIndexInRow + 1})`)); // all cells in the same col
+	const cellIndexInCol = cellColmates.map(item => +item.textContent).indexOf(cellValue); // index of 'cell' in the same col
 
-        // Add light-background highlighter to the relevant row
-        if (Number(cellItem.textContent) < Number(tdCell.textContent)) {
-            cellItem.classList.add(domStrings.lightBlueBg);
-        }
-    }
+	// highlight corresponding cells in the same row and column
+	/* prettier-ignore */
+	[...cellRowmates, ...cellColmates].forEach(item => (+item.textContent < cellValue) && highlightCell(item));
 
-    // Index of the current 'td' element
-    const currentTdIndex = rowmates.indexOf(tdCell.textContent);
-
-    // Add light-background highlighter to the relevant cols
-    for (let i = 1; i < 9; i++) {
-        if (Number(trs[i].getElementsByTagName('td')[currentTdIndex].textContent) < Number(tdCell.textContent)) {
-            trs[i].getElementsByTagName('td')[currentTdIndex].classList.add(domStrings.lightBlueBg);
-        }
-    }
-
-    // Select relevant 'th' element based on the index of the current 'td' element
-    if (toggleState === 'add') {
-        document.getElementById(domStrings.headerNumbersRow).getElementsByTagName('th')[(currentTdIndex + 1)].classList.add(domStrings.selectedClass);
-    } else if (toggleState === 'remove') {
-        document.getElementById(domStrings.headerNumbersRow).getElementsByTagName('th')[(currentTdIndex + 1)].classList.remove(domStrings.selectedClass);
-
-        // Remove light-background highlighter from all cols and rows
-        for (const cellItem of siblingTds) {
-            cellItem.classList.remove(domStrings.lightBlueBg);
-        }
-        for (let i = 1; i < 9; i++) {
-            trs[i].getElementsByTagName('td')[currentTdIndex].classList.remove(domStrings.lightBlueBg);
-        }
-    }
+	// add 'selected-cell' class to corresponding cells in side and top headings
+	const tableSideHeadingCells = [...document.querySelectorAll('#tableBody tr th')];
+	selectCell(tabletableTopHeaderRowCells[cellIndexInRow + 1]);
+	selectCell(tableSideHeadingCells[cellIndexInCol]);
 }
 
-// Loop through all 'td' elements for 'mouseenter' and 'mouseleave' events
-for (const td of tds) {
-    td.addEventListener('mouseenter', () => {
-        // Add 'selected' class to the current 'td' element
-        toggleSelectedOnTds(td, 'add');
-
-        // Add 'selected' class to the relevant 'th' element
-        toggleSelectedOnHeaderCells(td, 'add');
-    });
-
-    td.addEventListener('mouseleave', () => {
-        // Remove 'selected' class from the current 'td' element
-        toggleSelectedOnTds(td, 'remove');
-
-        // Remove 'selected' class from the relevant 'th' element
-        toggleSelectedOnHeaderCells(td, 'remove');
-    });
+function selectCell(cell) {
+	cell.classList.add('selected-cell');
 }
 
-// Loop through all 'th' elements for 'mouseenter' and 'mouseleave' events
-for (const th of ths) {
-    th.addEventListener('mouseenter', function () {
-        document.getElementById(domStrings.headerNumbersRow).getElementsByTagName('th')[(0)].classList.add(domStrings.selectedClass);
-    });
-    th.addEventListener('mouseleave', function () {
-        document.getElementById(domStrings.headerNumbersRow).getElementsByTagName('th')[(0)].classList.remove(domStrings.selectedClass);
-    });
+function highlightCell(cell) {
+	cell.classList.add('highlighted-cell');
+}
+
+function clearMarkedCells() {
+	document.querySelectorAll('.selected-cell, .highlighted-cell')?.forEach(cell => {
+		cell.classList.remove('selected-cell', 'highlighted-cell');
+	});
 }
